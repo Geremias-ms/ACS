87e8bf4e31f188aea82f6ce1d5bbc2a47718986a
Geremias Martins <geremias@appura.local>
2020-03-12 11:21:09 -0300

    ac02

--- ac02_test.py
+++ ac02_test.py
@@ -1,22 +1,74 @@
+# GEREMIAS MARTINS DOS SANTOS RA:1901844
+# NATALIA
+
 import ac02
 
 try:
-    lista = ["Marcílio dos Santos", "marcilio@email.com", "DESENVOLVEDOR", 5000.00]
+    lista = ["Marcílio Santos", "marcilio@email.com", "DESENVOLVEDOR", 5000.00]
     test = ac02.salario_liquido(lista)
     assert test == 4000.00
-    print("Correto")
+    print("Teste 1 Desnvolvedor: Correto")
 except AssertionError:
-    print("Erro")
+    print("Teste 1 Desenvolvedor: Erro")
     print("Retorno: ", test)
     print("Esperado: ", 4000.00)
-    
+
 try:
-    lista = ["Marinalva Aparecida", "marizinha@email.com", "DESENVOLVEDOR", 1500.00]
+    lista = ["Marcia Souza", "marizinha@email.com", "DESENVOLVEDOR", 1500.00]
     test = ac02.salario_liquido(lista)
     assert test == 1350.00
-    print("Correto")
+    print("Teste 2 Desenvolvedor: Correto")
 except AssertionError:
-    print("Erro")
+    print("Teste 2 Desenvolvedor: Erro")
     print("Retorno: ", test)
     print("Esperado: ", 1350.00)
-    
+
+try:
+    lista = ["Marcílio Santos", "marcilio@email.com", "ANALISTA", 4200.00]
+    test = ac02.salario_liquido(lista)
+    assert test == 3150.00
+    print("Teste 1 Analista: Correto")
+except AssertionError:
+    print("Teste 1 Analista: Erro")
+    print("Retorno: ", test)
+    print("Esperado: ", 3150.00)
+
+try:
+    lista = ["Marcia Souza", "marizinha@email.com", "ANALISTA", 3000.00]
+    test = (ac02.salario_liquido(lista))
+    assert test == 2550.00
+    print("Teste 2 Analista: Correto")
+except AssertionError:
+    print("Teste 2 Analista: Erro")
+    print("Retorno: ", test)
+    print("Esperado: ", 2550.00)
+
+try:
+    lista = ["Marcia Souza", "marizinha@email.com", "GERENTE", 8000.00]
+    test = (ac02.salario_liquido(lista))
+    assert test == 5600.00
+    print("Teste 1 Gerente: Correto")
+except AssertionError:
+    print("Teste 1 Gerente: Erro")
+    print("Retorno: ", test)
+    print("Esperado: ", 5600.00)
+
+try:
+    lista = ["Marcílio Santos", "marcilio@email.com", "GERENTE", 5100.00]
+    test = ac02.salario_liquido(lista)
+    assert test == 4080.00
+    print("Teste 2 Gerente: Correto")
+except AssertionError:
+    print("Teste 2 Gerente: Erro")
+    print("Retorno: ", test)
+    print("Esperado: ", 4080.00)
+
+try:
+    lista = ["Marcílio Santos", "marcilio@email.com", "Analista", 5100.00]
+    test = ac02.salario_liquido(lista)
+    assert test == 4080.00
+    print("Teste Gerente: Correto")
+except AssertionError:
+    print("Teste Gerente: Erro")
+    print("Retorno: ", test)
+    print("Esperado: ", 4080.00)
