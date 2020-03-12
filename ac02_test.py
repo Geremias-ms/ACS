d6bc70d04749486d1d15246cc8941d9adc3daf31
Geremias Martins dos Santos <1901844@SALAS.AULAS>
2020-03-11 22:24:52 -0300

    ac02test

--- ac02_test.py
+++ ac02_test.py
@@ -1,3 +1,22 @@
-lista = ["Marcílio dos Santos", "marcilio@email.com", "DESENVOLVEDOR", 5000.00]
-print(lista[2])
-print(lista[3])
+import ac02
+
+try:
+    lista = ["Marcílio dos Santos", "marcilio@email.com", "DESENVOLVEDOR", 5000.00]
+    test = ac02.salario_liquido(lista)
+    assert test == 4000.00
+    print("Correto")
+except AssertionError:
+    print("Erro")
+    print("Retorno: ", test)
+    print("Esperado: ", 4000.00)
+    
+try:
+    lista = ["Marinalva Aparecida", "marizinha@email.com", "DESENVOLVEDOR", 1500.00]
+    test = ac02.salario_liquido(lista)
+    assert test == 1350.00
+    print("Correto")
+except AssertionError:
+    print("Erro")
+    print("Retorno: ", test)
+    print("Esperado: ", 1350.00)
+    
